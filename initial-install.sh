993befc4c885e513c34044af0c178e6536a3e4de
Wayne F <wayne.fagin@wearefarm.com>
2020-03-21 10:24:10 +0000

    Update pgadmin install script

--- initial-install.sh
+++ initial-install.sh
@@ -160,8 +160,8 @@ echo "Installation and Configuration of Initial Packages Should Now Be Complete"
 echo '*************************************************************************'
 
 # Set up pgadmin4
-sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
 sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
 sudo apt update
 sudo apt install pgadmin4 pgadmin4-apache2
 echo '***************************************'
